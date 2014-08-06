346bc61866683e18bd028e8f272b3770ba69e726
Mike Schneider <schneider707@gmail.com>
2014-08-05 20:42:22 -0700

    Added nav highlighting.

--- js/main.js
+++ js/main.js
@@ -7,7 +7,8 @@ function set_active_link(){
     var page = get_page_name(document.URL);
     var links = document.getElementsByTagName('a');
 
-    for (var i = 0; i < links.length; i++) {
+    // hack: i starts at 1 in order to skip branding
+    for (var i = 1; i < links.length; i++) {
         var url_page = get_page_name(links[i].href);
         if (url_page == page){
             var parent_node = links[i].parentNode;
